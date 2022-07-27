fcbd8303762b92d236fab48c145e9d2c7fc89f54
warren jarron <98108022+warrenjarron@users.noreply.github.com>
2022-07-27 17:19:01 +0200

    update

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var messageArray = ["Typewriter Effect"];
+var textPosition = 0;
+var speed = 100;
+typewriter = () => {
+document.querySelector("#message").
+innerHTML = messageArray [0].substring(0,
+    textPosition) + "<span>\u25ae</span>"
+
+    if(textPosition++ != messageArray[0].length)
+    setTimeout(typewriter, speed);
+
+}
+
+window.addEventListener("load", typewriter);
